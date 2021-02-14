2ae9ac000ceae9c3f86f271ba3b67c27157efed2
SergeyShynkarenko <serg.shunkarenko@gmail.com>
2021-02-14 13:21:31 +0300

    fix menu

--- app/js/app.js
+++ app/js/app.js
@@ -3,14 +3,12 @@ $(function() {
 	$('.hamburger').on('click', function() {
 		$('.hamburger').toggleClass('is-active');
 		$('.menu__list').toggleClass('menu__list--active');
+		$('html').toggleClass('active');
 	});
 	$('.menu__list').on('click', function() {
 		$('.hamburger').removeClass('is-active');
 		$('.menu__list').removeClass('menu__list--active');
 	});
 
-	$('.hamburger').on('click', function() {
-		$('html').toggleClass('active');
-	});
 	
 });
